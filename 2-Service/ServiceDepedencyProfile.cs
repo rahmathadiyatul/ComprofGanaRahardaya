d2b7b564c60bb33b91ec20bcca710008bae08a8a
Rahmat Hadiyatul Akbar <rahmathadiyatulakbar@gmail.com>
2023-03-16 01:16:12 +0700

    Add files via upload
    BE

---
+++ 2-Service/ServiceDepedencyProfile.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SoupLocal._2_Service
+{
+    public class ServiceDepedencyProfile
+    {
+        public static void Register(IConfiguration configuration, IServiceCollection services)
+        {
+
+        }
+    }
+}
---
+++ 3-Data/DataDepedencyProfile.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SoupLocal._3_Data
+{
+    public class DataDepedencyProfile
+    {
+        public static void Register(IConfiguration configuration, IServiceCollection services)
+        {
+
+        }
+    }
+}
